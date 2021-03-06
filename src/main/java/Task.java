b98c73e9b89ec115c3e62b2dea271e11316af677
Абдуллабеков <abdullabekov@Air-Dina.Dlink>
2021-03-07 02:21:39 +0300

    first commit

---
+++ src/main/java/Task.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> adresses = getAdresses(scanner);
+        HashMap<String,ArrayList<String>> sortedAdresses = sort(adresses);
+        String resultString = getResultString(sortedAdresses);
+        System.out.println(resultString);
+    }
+    static ArrayList<String> getAdresses (Scanner scanner) {
+        ArrayList<String> adresses = new ArrayList<String>();
+        int lenght = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < lenght; i++){
+            adresses.add(scanner.nextLine());
+        }
+        return adresses;
+    }
+    static HashMap<String,ArrayList<String>> sort (ArrayList<String> adresses) {
+        HashMap<String,ArrayList<String>> sortedAdresses = new HashMap<>();
+        for (String adress : adresses) {
+            String newAdress = adress.toLowerCase();
+            String[] arrayAdress = newAdress.split("@");
+            if (arrayAdress[1].equals("gmail.com")) {
+                arrayAdress[0] = arrayAdress[0].replaceAll("\\.","");
+                if (arrayAdress[0].contains("+")) {
+                arrayAdress[0] = arrayAdress[0].substring(0, arrayAdress[0].indexOf("+"));
+                }
+                newAdress = arrayAdress[0] +"@" + arrayAdress[1];
+            }
+            if (sortedAdresses.containsKey(newAdress)) {
+                sortedAdresses.get(newAdress).add(adress);
+            } else {
+                sortedAdresses.put(newAdress, new ArrayList<String>());
+                sortedAdresses.get(newAdress).add(adress);
+            }
+        }
+        return sortedAdresses;
+    }
+    static String getResultString (HashMap<String,ArrayList<String>> sortedAdresses) {
+        String resultString = sortedAdresses.size() + "\n";
+        for (Map.Entry<String,ArrayList<String>> entry : sortedAdresses.entrySet()) {
+            resultString += entry.getValue().size() + " ";
+            for (String str : entry.getValue()) {
+                resultString += str + " ";
+            }
+            resultString += "\n";
+        };
+        return resultString;
+    }
+}
---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,63 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TaskTest {
+    static ArrayList<String> expectedAdresses;
+    HashMap<String,ArrayList<String>> expectedSortedAdresses;
+    @Before
+    public void initializer () {
+        expectedAdresses = new ArrayList<String>();
+        expectedAdresses.add("DEVELOPER.@gmail.com");
+        expectedAdresses.add("t+es+ter@GMAIL.COM");
+        expectedAdresses.add("T@bmail.com");
+        expectedAdresses.add("a@gmail.com.ru");
+        expectedAdresses.add("D.eveloper@Gmail.Com");
+        expectedAdresses.add("a+b@gmail.com.ru");
+        expectedSortedAdresses = new HashMap<>();
+        expectedSortedAdresses.put("t@bmail.com", new ArrayList<String>());
+        expectedSortedAdresses.put("t@gmail.com", new ArrayList<String>());
+        expectedSortedAdresses.put("a@gmail.com.ru", new ArrayList<String>());
+        expectedSortedAdresses.put("a+b@gmail.com.ru", new ArrayList<String>());
+        expectedSortedAdresses.put("developer@gmail.com", new ArrayList<String>());
+        expectedSortedAdresses.get("t@bmail.com").add("T@bmail.com");
+        expectedSortedAdresses.get("t@gmail.com").add("t+es+ter@GMAIL.COM");
+        expectedSortedAdresses.get("a@gmail.com.ru").add("a@gmail.com.ru");
+        expectedSortedAdresses.get("a+b@gmail.com.ru").add("a+b@gmail.com.ru");
+        expectedSortedAdresses.get("developer@gmail.com").add("DEVELOPER.@gmail.com");
+        expectedSortedAdresses.get("developer@gmail.com").add("D.eveloper@Gmail.Com");
+    }
+    @Test
+    public void getAdressesTest () {
+        Scanner scanner = new Scanner(
+                "6\n" +
+                "DEVELOPER.@gmail.com\n" +
+                "t+es+ter@GMAIL.COM\n" +
+                "T@bmail.com\n" +
+                "a@gmail.com.ru\n" +
+                "D.eveloper@Gmail.Com\n" +
+                "a+b@gmail.com.ru");
+        ArrayList<String> adresses = Task.getAdresses(scanner);
+        Assert.assertEquals(expectedAdresses, adresses);
+    }
+    @Test
+    public void sortTest () {
+        HashMap<String,ArrayList<String>> sortedAdresses = Task.sort(expectedAdresses);
+        Assert.assertEquals(expectedSortedAdresses,sortedAdresses);
+    }
+    @Test
+    public void getResultStringTest () {
+        String expectedString = "5\n" +
+                "1 T@bmail.com \n" +
+                "1 t+es+ter@GMAIL.COM \n" +
+                "1 a@gmail.com.ru \n" +
+                "1 a+b@gmail.com.ru \n" +
+                "2 DEVELOPER.@gmail.com D.eveloper@Gmail.Com \n";
+        String string = Task.getResultString(expectedSortedAdresses);
+        Assert.assertEquals(expectedString, string);
+    }
+}
